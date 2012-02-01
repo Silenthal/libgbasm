c50cfe2e013336c276327c4bbb2fbf5bc9fa9909
Silenthal <Silenthal@localhost>
2012-02-01 12:01:21 -0500

    Completed main method, added static lookup tables, and retargeted project towards .NET 2.0

--- InstructionUnit.cs
+++ InstructionUnit.cs
@@ -1,64 +1,24 @@
-﻿using System.Collections.Generic;
-
-namespace LibGBasm
+﻿namespace LibGBasm
 {
-	public enum InstructionType { adc, add, and, bit, call, ccf, cb, cp, cpl, ei, daa, data, dec, di, halt, inc, jp, jr, ld, ldi, ldd, ldhl, nop, or, pop, push, rla, rlc, rlca, res, ret, reti, rot, rl, rr, rra, rrc, rrca, rst, sbc, scf, set, shft, sla, sra, srl, stop, sub, swap, xor }
-	public enum ArgumentType { None, MemMap, RegisterDoubleMemMap, RegisterSingleMemMap, Conditional, Register, RegisterPair, Byte, Word, Offset }
-	public enum LabeledArgument { None, First, Second }
-	public enum RegisterSingle { a, b, c, d, e, h, l }
-	public enum RegisterDouble { af, bc, de, hl, sp }
-	public enum Conditional { nz, z, nc, c }
+	public enum InstructionType { invalid, adc, add, and, bit, call, ccf, cb, cp, cpl, ei, daa, dec, di, halt, inc, jp, jr, ld, ldi, ldd, ldhl, nop, or, pop, push, res, ret, reti, rl, rla, rlc, rlca, rot, rr, rra, rrc, rrca, rst, sbc, scf, set, sla, sra, srl, stop, sub, swap, xor }
 
-	internal class InstructionTypes
-	{
-		public static Dictionary<InstructionType, string> instStringTranslationTable = new Dictionary<InstructionType, string>
-		{
-			{ InstructionType.adc, "adc" }, 
-			{ InstructionType.add, "add" }, 
-			{ InstructionType.and, "and" }, 
-			{ InstructionType.bit, "bit" }, 
-			{ InstructionType.call, "call" }, 
-			{ InstructionType.cb, "cb" }, 
-			{ InstructionType.ccf, "ccf" }, 
-			{ InstructionType.cpl, "cpl" }, 
-			{ InstructionType.daa, "daa" }, 
-			{ InstructionType.dec, "dec" }, 
-			{ InstructionType.di, "di" }, 
-			{ InstructionType.ei, "ei" }, 
-			{ InstructionType.halt, "halt" }, 
-			{ InstructionType.inc, "inc" }, 
-			{ InstructionType.jp, "jp" }, 
-			{ InstructionType.jr, "jr" }, 
-			{ InstructionType.ld, "ld" }, 
-			{ InstructionType.nop, "nop" }, 
-			{ InstructionType.or, "or" }, 
-			{ InstructionType.pop, "pop" }, 
-			{ InstructionType.push, "push" }, 
-			{ InstructionType.res, "res" }, 
-			{ InstructionType.ret, "ret" }, 
-			{ InstructionType.rot, "rot" }, 
-			{ InstructionType.rst, "rst" }, 
-			{ InstructionType.sbc, "sbc" }, 
-			{ InstructionType.scf, "scf" }, 
-			{ InstructionType.set, "set" }, 
-			{ InstructionType.shft, "shft" }, 
-			{ InstructionType.cp, "special" }, 
-			{ InstructionType.stop, "stop" }, 
-			{ InstructionType.sub, "sub" }, 
-			{ InstructionType.swap, "swap" }, 
-			{ InstructionType.xor, "xor" }, 
-		};
-	}
+	public enum ArgumentType { None, MemMap, RegisterDoubleMemMap, RegisterSingleMemMap, Conditional, Register, RegisterPair, Byte, MemMapFFByte, Word, Offset }
+
+	public enum RegisterSingle { a, b, c, d, e, h, l }
+
+	public enum RegisterDouble { af, bc, de, hl, sp }
+
+	public enum Conditional { nz, z, nc, c }
 
 	public struct InstructionUnit
 	{
 		public int fileOffset;
+		public int virtualOffset;
+		public int instSize;
 		public InstructionType instType;
 		public int ArgCount;
-		public LabeledArgument labeledArg;
 		public Argument arg1;
 		public Argument arg2;
-		public int dataSize;
 	}
 
 	public struct Argument
@@ -66,8 +26,6 @@ namespace LibGBasm
 		public ArgumentType argType;
 		public byte byteArg;
 		public ushort shortArg;
-		public int offsetArg;
-		public string stringArg;
 		public RegisterSingle rsArg;
 		public RegisterDouble rdArg;
 		public Conditional cArg;
---
+++ LibGBasm.cs
@@ -0,0 +1,21 @@
+﻿namespace LibGBasm
+{
+	public class LibGBasm
+	{
+		public static bool GetZ80Instruction(byte[] binaryFile, int virtualOffset, int fileOffset, ref InstructionUnit output)
+		{
+			output = new InstructionUnit();
+			if (binaryFile == null || fileOffset > binaryFile.Length - 1) return false;
+			byte inst = binaryFile[fileOffset];
+			if (inst == 0xCB)
+			{
+				if (fileOffset >= binaryFile.Length - 2) return false;
+				output = Z80Instructions.CBInstructionUnitTable[binaryFile[fileOffset]];
+			}
+			else output = Z80Instructions.InstructionUnitTable[binaryFile[fileOffset]];
+			output.fileOffset = fileOffset;
+			output.virtualOffset = fileOffset + virtualOffset;
+			return true;
+		}
+	}
+}
